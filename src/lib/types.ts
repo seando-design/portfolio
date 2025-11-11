b2e7f6146390b2286c5e494e65ca9efac7148ebd
Sean Do <sean.ngocdo01@gmail.com>
2025-11-12 04:30:18 +1100

    finished homepage, set up project page

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,6 +1,21 @@
+import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import type { PortableTextBlock } from "@portabletext/types";
+
 export interface Project {
-	title: string;
-	slug?: { current: string };
-	projectType: string[];
-	imageUrl?: string;
+  title: string;
+  slug: { current: string };
+  projectType: string[];
+  thumbnail: SanityImageSource;
+}
+
+export interface ProjectDetailed {
+  title: string;
+  slug: { current: string };
+  projectType: string[];
+  description: PortableTextBlock[];
+  images: {
+    asset: SanityImageSource;
+    alt: string;
+  }[];
+  layout: string;
 }
--- studio/schemaTypes/project.ts
+++ studio/schemaTypes/project.ts
@@ -4,11 +4,36 @@ export default defineType({
   name: 'project',
   title: 'Project',
   type: 'document',
+  preview: {
+    select: {
+      title: 'title',
+      thumbnail: 'thumbnail',
+      rating: 'rating',
+    },
+    prepare({title, thumbnail, rating}) {
+      return {
+        title: title || 'Untitled',
+        subtitle: 'Rating: ' + rating,
+        media: thumbnail,
+      }
+    },
+  },
+  groups: [
+    {
+      name: 'preview',
+      title: 'Preview',
+    },
+    {
+      name: 'details',
+      title: 'Details',
+    },
+  ],
   fields: [
     defineField({
       name: 'title',
       title: 'Title',
       type: 'string',
+      group: ['preview', 'details'],
     }),
     defineField({
       name: 'slug',
@@ -18,18 +43,20 @@ export default defineType({
         source: 'title',
         maxLength: 96,
       },
+      group: ['preview'],
     }),
     defineField({
       name: 'projectType',
       title: 'Project Type',
       type: 'array',
       of: [{type: 'string'}],
+      group: ['preview', 'details'],
       options: {
         list: [
           {title: 'Web', value: 'web'},
           {title: 'Publication', value: 'publication'},
           {title: 'Identity', value: 'identity'},
-          {title: 'Type Design', value: 'type'},
+          {title: 'Type Design', value: 'type design'},
         ],
         layout: 'grid',
       },
@@ -38,7 +65,61 @@ export default defineType({
       name: 'thumbnail',
       title: 'Thumbnail',
       type: 'image',
+      group: 'preview',
       options: {hotspot: true},
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      description: 'Remember to italicise links',
+      type: 'array',
+      of: [{type: 'block'}],
+      group: 'details',
+    }),
+    defineField({
+      name: 'images',
+      title: 'Images',
+      group: 'details',
+      type: 'array',
+      of: [
+        defineField({
+          name: 'image',
+          title: 'Image',
+          type: 'image',
+          options: {
+            hotspot: true, // enables crop & focus point
+          },
+          fields: [
+            defineField({
+              name: 'alt',
+              title: 'Alt Text',
+              type: 'string',
+            }),
+          ],
+        }),
+      ],
+    }),
+    defineField({
+      name: 'rating',
+      type: 'number',
+      group: 'details',
+      description: 'Rate 0-100',
+      validation: (rule) =>
+        rule.min(0).error('Value must be >= 0').max(100).error('Value must be <=100'),
+    }),
+    defineField({
+      name: 'layout',
+      title: 'Images Layout',
+      type: 'string',
+      group: 'details',
+      options: {
+        list: [
+          {title: 'Two-Column', value: '2-col'},
+          {title: 'One-Column', value: '1-col'},
+        ],
+        layout: 'radio',
+      },
+      initialValue: '2-col',
     }),
   ],
 })
